79d5e37a7d349dce530279d781d3df17f844e123
mborsetti <mike@borsetti.com>
2020-11-13 14:20:57 +0800

    Travis fix for 3.0.0

--- tests/test_-flake8.py
+++ tests/test_-flake8.py
@@ -10,25 +10,3 @@ def test_flake8():
     py_files = [y for x in os.walk(os.path.abspath('webchanges')) for y in glob(os.path.join(x[0], '*.py'))]
     report = style_guide.check_files(py_files)
     assert report.get_statistics('E') == [], 'Flake8 found violations'
-
-
-def test_print_package_versions():
-    import importlib
-    print('\nPackage versions:')
-    for package in sorted((
-            'appdirs', 'cssselect', 'html2text', 'lxml', 'markdown2', 'minidb', 'yaml', 'requests', 'colorama',
-            'pyppeteer', 'bs4', 'jsbeautifier', 'cssbeautifier', 'pdf2text', 'pytesseract', 'PIL', 'vobject',
-            'chump', 'pushbullet', 'matrix_client', 'aioxmpp', 'msgpack', 'redis', 'keyring', 'docutils', 'pytest',
-            'coverage', 'flake8', 'flake8_import_order', 'docutils', 'sphinx', 'sphinx_rtd_theme'), key=str.casefold):
-        if package not in ('keyring', 'pdf2text', 'pytesseract', 'vobject'):
-            try:
-                if package == 'msgpack':
-                    print(f"msgpack=={importlib.import_module(f'msgpack._version', 'version').version}")
-                elif package == 'cssbeautifier':
-                    print(f"cssbeautifier=="
-                          f"{importlib.import_module('cssbeautifier.__version__', '__version__').__version__}")
-                else:
-                    print(f"{package}=={importlib.import_module(package, '__version__').__version__}")
-            except ImportError:
-                print(f"{package} not installed")
-    print()
