40c21029832664b4860955ad490a9d91bf0cb6b5
mborsetti <mike@borsetti.com>
2020-11-14 14:55:58 +0800

    Test fix for 3.0.0

--- tests/test_-build_docs.py
+++ tests/test_-build_docs.py
@@ -7,6 +7,8 @@ def test_build_docs():
     """Test that Sphinx does not have any warnings or errors"""
     # readthedocs.io uses Python 3.8 so only run continuous integration in 3.8
     if not os.environ.get('CI') or (sys.version_info.major == 3 and sys.version_info.minor == 8):
-        stdout = subprocess.run('sphinx-build -M html ../docs ../docs/build'.split(),
-                                capture_output=True, check=True).stdout.decode()
+        run = subprocess.run('sphinx-build -M html ../docs ../docs/build'.split(), capture_output=True)
+        stderr = run.stderr.decode()
+        assert not stderr
+        stdout=run.stdout.decode()
         assert not any(x in stdout for x in ('WARNING', 'ERROR'))
