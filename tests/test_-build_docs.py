bdf93e81a90a3375dd044d3f0f539c214718b59e
mborsetti <mike@borsetti.com>
2020-12-28 20:52:24 +0800

    Fix travis CI

--- tests/test_-build_docs.py
+++ tests/test_-build_docs.py
@@ -4,12 +4,11 @@ import sys
 
 
 def test_build_docs():
-    """Test that Sphinx does not have any warnings or errors"""
+    """Test that Sphinx build does not have any warnings or errors"""
     # readthedocs.io uses Python 3.8 so only run continuous integration in 3.8
     if not os.environ.get('CI') or (sys.version_info.major == 3 and sys.version_info.minor == 8):
-        if not os.environ.get('CI'):  # TODO "\nApplication error:\nconfig directory doesn't contain a conf.py file
-            run = subprocess.run('sphinx-build -M html ../docs ../docs/_build'.split(), capture_output=True)
-            stderr = run.stderr.decode()
-            assert not stderr
-            stdout = run.stdout.decode()
-            assert not any(x in stdout for x in ('WARNING', 'ERROR'))
+        run = subprocess.run('sphinx-build -M html ../docs ../docs/_build'.split(), capture_output=True)
+        stderr = run.stderr.decode()
+        assert not stderr
+        stdout = run.stdout.decode()
+        assert not any(x in stdout for x in ('WARNING', 'ERROR'))
