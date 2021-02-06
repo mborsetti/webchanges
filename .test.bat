d2eee9314a14c2737c1e22c11bd75f325436d2b0
mborsetti <mike@borsetti.com>
2021-02-06 18:31:35 +0800

    fixed flake8-import-order

--- .test.bat
+++ .test.bat
@@ -5,9 +5,9 @@ pip install -U -r requirements.txt
 if not errorlevel 0 pause
 pip install -U -r requirements_testing.txt
 if not errorlevel 0 pause
-python -m pytest -v
-if not errorlevel 0 pause
 pre-commit autoupdate
 if not errorlevel 0 pause
 pre-commit run -a
+if not errorlevel 0 pause
+python -m pytest -v
 if not errorlevel 0 pause
